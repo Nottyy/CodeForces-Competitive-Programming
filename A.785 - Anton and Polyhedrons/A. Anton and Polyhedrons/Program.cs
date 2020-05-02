c71c14434a5878f9de24548193a8a9e32d8d416e
Arsenal14 <antonio.marinovvvv@gmail.com>
2020-05-02 15:47:45 +0800

    A785 - Anton and Polyhedrons

---
+++ A.785 - Anton and Polyhedrons/A. Anton and Polyhedrons/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace A._Anton_and_Polyhedrons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string figure = Console.ReadLine().Trim().ToLower();
+                switch (figure)
+                {
+                    case "tetrahedron":
+                        sum += 4;
+                        break;
+                    case "cube":
+                        sum += 6;
+                        break;
+                    case "octahedron":
+                        sum += 8;
+                        break;
+                    case "dodecahedron":
+                        sum += 12;
+                        break;
+                    case "icosahedron":
+                        sum += 20;
+                        break;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
