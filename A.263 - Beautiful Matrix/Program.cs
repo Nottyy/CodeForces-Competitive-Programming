1d7af873de1d11afcf9317a7caad2f450bbae51e
Arsenal14 <antonio.marinovvvv@gmail.com>
2020-05-02 16:09:59 +0800

    A263 - Beautiful Matrix

---
+++ A.263 - Beautiful Matrix/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace A._263___Beautiful_Matrix
+{
+    class Program
+    {
+        public const int BeautifulMatrixRow = 3;
+        public const int BeautifulMatrixCol = 3;
+       
+        static void Main(string[] args)
+        {
+            int numberOneInitialRow = 0;
+            int numberOneInitialCol = 0;
+            for (int i = 1; i <= 5; i++)
+            {
+                var line = String.Join("", Console.ReadLine().Split(' '));
+                if (line.Contains("1"))
+                {
+                    numberOneInitialRow = i;
+                    numberOneInitialCol = line.IndexOf("1") + 1;
+                    break;
+                }
+            }
+
+            var result = Math.Abs(BeautifulMatrixCol - numberOneInitialCol) + Math.Abs(BeautifulMatrixRow - numberOneInitialRow);
+            Console.WriteLine(result);
+        }
+    }
+}
