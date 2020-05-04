05c7ccd3939c2ebb9db0a88476e89856e9810103
Arsenal14 <antonio.marinovvvv@gmail.com>
2020-05-04 19:36:59 +0800

    900 Complexity - Construct the String

---
+++ 900 -- B1335 - Construct the String/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace _900____B1335___Construct_the_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine().Split(' ');
+                string result = ConstructString(line, sb);
+                Console.WriteLine(result);
+            }
+        }
+
+        private static string ConstructString(string[] line, StringBuilder sb)
+        {
+            sb.Clear();
+
+            var length = int.Parse(line[0]);
+            var lengthOfSubstring = int.Parse(line[1]);
+            var distinctLetters = int.Parse(line[2]);
+            int counter = 0;
+
+            for (int i = 0; i < lengthOfSubstring; i++)
+            {                
+                if (counter >= distinctLetters)
+                {
+                    counter--;
+                }
+                sb.Append((char)(counter + 97));
+                counter++;
+            }
+            var str = sb.ToString();
+
+            for (int i = 1; i < length / lengthOfSubstring; i++)
+            {
+                sb.Append(str);
+            }
+            if (length % lengthOfSubstring != 0)
+            {
+                for (int i = 0; i < length % lengthOfSubstring; i++)
+                {
+                    sb.Append(sb[i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
