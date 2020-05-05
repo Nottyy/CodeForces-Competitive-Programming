37682ad8ece962bb8b2a0ac1156598761376824c
Arsenal14 <antonio.marinovvvv@gmail.com>
2020-05-05 15:49:54 +0800

    800 Complexity - SoldiersAndBananas

---
+++ 800 -- A546 - SoldierAndBananas/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _800____A546___SoldierAndBananas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int costOfFirstBanana = input[0];
+            int initialSoldierDollars = input[1];
+            int bananasSoliderWants = input[2];
+
+            var totalCostOfBananas = costOfFirstBanana * CalculateSumOfFirstGivenNIntegers(bananasSoliderWants);
+
+            var dollarsToBorrow = 0;
+            if (totalCostOfBananas > initialSoldierDollars)
+            {
+                dollarsToBorrow = totalCostOfBananas - initialSoldierDollars;
+            }
+
+            Console.WriteLine(dollarsToBorrow);
+        }
+
+        static int CalculateSumOfFirstGivenNIntegers(int N)
+        {
+            // Sum of N given integers = N * (N + 1) / 2;
+            int result = (N * (N + 1)) / 2;
+
+            return result;
+        }
+    }
+}
