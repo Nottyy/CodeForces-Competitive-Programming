bfe6eb5a156d8825a2ecc36abb801cac1230594d
Arsenal14 <antonio.marinovvvv@gmail.com>
2020-05-05 15:04:18 +0800

    800 Complexity - WayTooLongWords

---
+++ 800 -- A71 - WayTooLongWords/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace _800____A71___WayTooLongWords
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < n; i++)
+            {
+                var word = Console.ReadLine();
+
+                if (word.Length > 10)
+                {
+                    sb.Append(word[0]);
+                    sb.Append(word.Length - 2);
+                    sb.Append(word[word.Length - 1]);
+                    sb.AppendLine();
+                }
+                else
+                {
+                    sb.AppendLine(word);
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
