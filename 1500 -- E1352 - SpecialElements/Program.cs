587c74a677d2b86b2a9414d35dbc24540d06aae7
Arsenal14 <antonio.marinovvvv@gmail.com>
2020-05-12 00:52:54 +0800

    1500 complexity - SpecialElements - implementation

---
+++ 1500 -- E1352 - SpecialElements/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1500____E1352___SpecialElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var arrLength = int.Parse(Console.ReadLine());
+                var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                var hashset = new HashSet<int>(input);
+                int specialNumbers = CalculateSpecialNumber(input, hashset);
+                Console.WriteLine(specialNumbers);
+            }
+        }
+
+        private static int CalculateSpecialNumber(int[] input, HashSet<int> hashset)
+        {
+            int specialNumbers = 0;
+            int currentSum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                currentSum = input[i];
+                for (int j = i + 1; j < input.Length; j++)
+                {
+                    currentSum += input[j];
+                    if(currentSum > input.Length)
+                    {
+                        break;
+                    }
+                    if (hashset.Contains(currentSum))
+                    {
+                        specialNumbers++;
+                    }
+                }
+            }
+
+            return specialNumbers;
+        }
+    }
+}
---
+++ 900 -- A903 - HungryStudentProblem/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _900____A903___HungryStudentProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int chickenChunks = int.Parse(Console.ReadLine());
+
+                if (chickenChunks % 3 == 0 || chickenChunks % 7 == 0 || chickenChunks % 10 == 0)
+                {
+                    Console.WriteLine("YES");
+                    continue;
+                }
+
+                if (chickenChunks > 7)
+                {
+                    if ((chickenChunks - 3) % 7 == 0 || (chickenChunks - 3) % 3 == 0 || (chickenChunks - 3) % 10 == 0 || 
+                        (chickenChunks - 7) % 7 == 0 || (chickenChunks - 7) % 3 == 0 ||(chickenChunks - 7) % 10 == 0)
+                    {
+                        Console.WriteLine("YES");
+                        continue;
+                    }
+                }
+                Console.WriteLine("NO");
+            }
+
+        }
+    }
+}
