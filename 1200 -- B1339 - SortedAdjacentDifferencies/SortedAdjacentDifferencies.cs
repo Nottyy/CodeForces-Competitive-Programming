03b29d1744fda9489f3b7262b4041e8f67ad60fc
Arsenal14 <antonio.marinovvvv@gmail.com>
2020-05-13 01:13:02 +0800

    1200 Complexity - SortedAdjacencyDifferencies - Array Sort, Implementation

---
+++ 1200 -- B1339 - SortedAdjacentDifferencies/SortedAdjacentDifferencies.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Linq;
+
+namespace _1200____B1339___SortedAdjacentDifferencies
+{
+    class SortedAdjacentDifferencies
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int arrLength = int.Parse(Console.ReadLine());
+                var arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                var rearrangedArray = new int[arrLength];
+                Array.Sort(arr);
+
+                var counter = 0;
+                var counterLength = arrLength % 2 == 0 ? arrLength / 2 : arrLength / 2 + 1;
+                while (counter < counterLength)
+                {
+                    rearrangedArray[arrLength - 1 - (counter * 2)] = arr[arrLength - 1 - counter];
+                    if (arrLength % 2 != 0 && counter == counterLength - 1)
+                    {
+                        break;
+                    }
+                    rearrangedArray[arrLength - 1 - (counter * 2) - 1] = arr[counter];
+                    counter++;
+                }
+
+                Console.WriteLine(string.Join(" ", rearrangedArray));
+            }
+        }
+    }
+}
--- 1500 -- E1352 - SpecialElements/Program.cs
+++ 1500 -- E1352 - SpecialElements/Program.cs
@@ -6,6 +6,8 @@ namespace _1500____E1352___SpecialElements
 {
     class Program
     {
+        //9
+        //3 1 4 1 5 9 2 6 5
         static void Main(string[] args)
         {
             var n = int.Parse(Console.ReadLine());
@@ -14,13 +16,21 @@ namespace _1500____E1352___SpecialElements
             {
                 var arrLength = int.Parse(Console.ReadLine());
                 var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-                var hashset = new HashSet<int>(input);
-                int specialNumbers = CalculateSpecialNumber(input, hashset);
+                var frequencies = new int[arrLength + 1];
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    var currentNum = input[j];
+                    frequencies[currentNum]++;
+                }
+
+                //var hashset = new HashSet<int>(input);
+                int specialNumbers = CalculateSpecialNumber(input, frequencies);
                 Console.WriteLine(specialNumbers);
             }
         }
 
-        private static int CalculateSpecialNumber(int[] input, HashSet<int> hashset)
+        private static int CalculateSpecialNumber(int[] input, int[] frequencies)
         {
             int specialNumbers = 0;
             int currentSum = 0;
@@ -30,13 +40,14 @@ namespace _1500____E1352___SpecialElements
                 for (int j = i + 1; j < input.Length; j++)
                 {
                     currentSum += input[j];
-                    if(currentSum > input.Length)
+                    if (currentSum > input.Length)
                     {
                         break;
                     }
-                    if (hashset.Contains(currentSum))
+                    if (frequencies[currentSum] > 0)
                     {
-                        specialNumbers++;
+                        specialNumbers += frequencies[currentSum];
+                        frequencies[currentSum] = 0;
                     }
                 }
             }
