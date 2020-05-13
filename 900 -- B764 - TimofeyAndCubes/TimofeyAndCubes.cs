431f8563d6cd470b47a0e049a66c8b7a6131f4f1
Arsenal14 <antonio.marinovvvv@gmail.com>
2020-05-14 01:25:17 +0800

    900 Complexity - TimofeyAndCubes - Sorting Array

---
+++ 900 -- B764 - TimofeyAndCubes/TimofeyAndCubes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _900____B764___TimofeyAndCubes
+{
+    class TimofeyAndCubes
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var changedOrderArr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] originalOrderArr = new int[n];
+
+            for (int i = 0; i < n / 2; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    originalOrderArr[i] = changedOrderArr[n - i - 1];
+                    originalOrderArr[n - i - 1] = changedOrderArr[i];
+                }
+                else
+                {
+                    originalOrderArr[i] = changedOrderArr[i];
+                    originalOrderArr[n - i - 1] = changedOrderArr[n - i - 1];
+                }
+                
+            }
+            if (n % 2 != 0)
+            {
+                originalOrderArr[n / 2] = changedOrderArr[n / 2];
+            }
+            Console.WriteLine(string.Join(" ", originalOrderArr));
+        }
+    }
+}
