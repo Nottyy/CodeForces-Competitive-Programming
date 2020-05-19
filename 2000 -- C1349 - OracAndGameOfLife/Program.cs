34d2ddc6cf5bab6656d7882f32955aad4b962be7
Arsenal14 <antonio.marinovvvv@gmail.com>
2020-05-19 17:20:52 +0800

    2000 complexity -  reimplemented the BFS

--- 2000 -- C1349 - OracAndGameOfLife/Program.cs
+++ 2000 -- C1349 - OracAndGameOfLife/Program.cs
@@ -1,23 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
+using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 
 namespace _2000____C1349___OracAndGameOfLife
 {
     public class Node
     {
-        public Node(int vertex, bool boolValue)
+        public Node(int vertex, bool boolValue, bool from)
         {
             Vertex = vertex;
             BoolValue = boolValue;
+            FromBoolValue = from;
         }
 
         public int Vertex { get; set; }
         public bool BoolValue { get; set; }
+        public bool FromBoolValue { get; set; }
     }
-    public class Program
+    class Program
     {
         static void Main()
         {
@@ -32,14 +33,14 @@ namespace _2000____C1349___OracAndGameOfLife
 
             for (int i = 0; i < queries; i++)
             {
-                var query = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                var query = Console.ReadLine().Split(' ').Select(long.Parse).ToArray();
                 var qRow = query[0];
                 var qCol = query[1];
                 var p = query[2];
 
-                int startIndex = qRow == 1 ? qCol : qCol + (i + 1- 1) * cols;
+                long startIndex = qRow == 1 ? qCol : qCol + (qRow- 1) * cols;
 
-                int res = BFSQueue(startIndex, vertices, !matrix[qRow, qCol], p);
+                long res = BFSQueue(startIndex, vertices, p);
 
                 if (res == -1)
                 {
@@ -49,30 +50,30 @@ namespace _2000____C1349___OracAndGameOfLife
                 {
                     if (p - res == 0)
                     {
-                        Console.WriteLine(matrix[qRow, qCol] == true ? 0 : 1);
+                        Console.WriteLine(matrix[qRow, qCol] == true ? 1 : 0);
                     }
                     else
                     {
                         if ((p - res) % 2 == 0)
                         {
-                            Console.WriteLine(matrix[qRow, qCol] == true ? 0 : 1);
+                            Console.WriteLine(matrix[qRow, qCol] == true ? 1 : 0);
                         }
                         else
                         {
-                            Console.WriteLine(matrix[qRow, qCol] == true ? 1 : 0);
+                            Console.WriteLine(matrix[qRow, qCol] == true ? 0 : 1);
                         }
                     }
                 }
             }
         }
 
-        private static int BFSQueue(int startVertex, LinkedList<Node>[] vertices, bool oppValue, int iteration)
+        private static long BFSQueue(long startVertex, LinkedList<Node>[] vertices, long iteration)
         {
             bool[] used = new bool[vertices.Length + 1];
             used[startVertex] = true;
-            var distances = new int[vertices.Length + 1];
+            var distances = new long[vertices.Length + 1];
             distances[startVertex - 1] = 0;
-            var queue = new Queue<int>();
+            var queue = new Queue<long>();
             queue.Enqueue(startVertex);
 
             while (queue.Count > 0)
@@ -88,7 +89,7 @@ namespace _2000____C1349___OracAndGameOfLife
 
                     distances[next.Vertex] = distances[vertex] + 1;
                     
-                    if (next.BoolValue == oppValue)
+                    if (next.BoolValue == next.FromBoolValue)
                     {
                         return distances[next.Vertex];
                     }
@@ -121,40 +122,24 @@ namespace _2000____C1349___OracAndGameOfLife
                     // up 1
                     if (i > 1)
                     {
-                        newMatrix[currentIndex].AddLast(new Node(currentIndex - cols, matrix[i - 1, j]));
+                        newMatrix[currentIndex].AddLast(new Node(currentIndex - cols, matrix[i - 1, j], matrix[i,j]));
                     }
                     // down 1
                     if (i <= cols - 1)
                     {
-                        newMatrix[currentIndex].AddLast(new Node(currentIndex + cols, matrix[i + 1, j]));
+                        newMatrix[currentIndex].AddLast(new Node(currentIndex + cols, matrix[i + 1, j], matrix[i, j]));
                     }
 
-                    // left 3
+                    // left 1
                     if (j > 1)
                     {
-                        newMatrix[currentIndex].AddLast(new Node(currentIndex - 1, matrix[i, j - 1]));
-                        if (i > 1)
-                        {
-                            newMatrix[currentIndex].AddLast(new Node(currentIndex - 1 - cols, matrix[i - 1, j - 1]));
-                        }
-                        if (i <= cols - 1)
-                        {
-                            newMatrix[currentIndex].AddLast(new Node(currentIndex - 1 + cols, matrix[i + 1, j - 1]));
-                        }
+                        newMatrix[currentIndex].AddLast(new Node(currentIndex - 1, matrix[i, j - 1], matrix[i, j]));
                     }
 
-                    // right 3
+                    // right 1
                     if (j <= cols - 1)
                     {
-                        newMatrix[currentIndex].AddLast(new Node(currentIndex + 1, matrix[i, j + 1]));
-                        if (i > 1)
-                        {
-                            newMatrix[currentIndex].AddLast(new Node(currentIndex + 1 - cols, matrix[i - 1, j + 1]));
-                        }
-                        if (i <= cols - 1)
-                        {
-                            newMatrix[currentIndex].AddLast(new Node(currentIndex + 1 + cols, matrix[i + 1, j + 1]));
-                        }
+                        newMatrix[currentIndex].AddLast(new Node(currentIndex + 1, matrix[i, j + 1], matrix[i, j]));
                     }
 
                 }
