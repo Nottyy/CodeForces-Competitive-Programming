2976296024d1a5efcf744cf16255ef1f9f1891a7
Arsenal14 <antonio.marinovvvv@gmail.com>
2020-05-19 22:46:43 +0800

    800 complexity - BrainsPhotos

---
+++ 800 -- A707 - BrainsPhotos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _800____A707___BrainsPhotos
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var n = input[0];
+            var m = input[1];
+            bool contains = false;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (Console.ReadLine().Split(' ').Any(x => x == "C" || x == "M" || x == "Y"))
+                {
+                    contains = true;
+                    break;
+                }
+            }
+
+
+            if (contains)
+            {
+                Console.WriteLine("#Color");
+            }
+            else
+            {
+                Console.WriteLine("#Black&White");
+            }
+        }
+    }
+}
