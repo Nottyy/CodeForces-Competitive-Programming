c62fa66afd89943a1ebb32f4c492833077132c58
Arsenal14 <antonio.marinovvvv@gmail.com>
2020-06-06 21:11:09 +0800

    800 complexity

---
+++ 800 -- A1358 - ParkLighting/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _800____A1358___ParkLighting
+{
+    class Program
+    {
+        static void Main()
+        {
+            var cases = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cases; i++)
+            {
+                var matrixSize = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                float res = (matrixSize[0] * matrixSize[1] + 1) / 2;
+                Console.WriteLine(res);
+            }
+        }
+    }
+}
