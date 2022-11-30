4c7265089b70123829af65b21918dc96804b7eda
Antonio <antonio.marinovvvv@gmail.com>
2022-11-30 18:19:35 +0200

    1000 - Exchange

---
+++ 1000 -- E1765 - Exchange/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1000____E1765___Exchange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputNum; i++)
+            {
+                var input = Console.ReadLine().Split(' ').Select(o => int.Parse(o)).ToArray();
+                var n = input[0];
+                var a = input[1];
+                var b = input[2];
+
+                if (a <= b)
+                {
+                    int remainder;
+                    int quotient = Math.DivRem(n, a, out remainder);
+                    Console.WriteLine(remainder == 0 ? quotient : quotient + 1);
+                }
+                else
+                {
+                    Console.WriteLine(1);
+                }
+            }
+        }
+    }
+}
