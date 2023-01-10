9f36c429307e69f4ddfe00dbae272da426f5d066
Antonio <antonio.marinovvvv@gmail.com>
2023-01-10 09:53:43 +0200

    fastpower

---
+++ FastPower/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FastPower(5,5));
+        }
+
+        static int FastPower(int n, int p)
+        {
+            if (p == 0)
+            {
+                return 1;
+            }
+
+            if (p % 2 == 1)
+            {
+                return n * FastPower(n, p - 1);
+            }
+
+            int half = FastPower(n, p / 2);
+            return half * half;
+
+            
+        }
+    }
+}
