4403a6ce3ed5ebcd828ab880f7240aa45fe8784d
Antonio <antonio.marinovvvv@gmail.com>
2023-01-10 15:39:34 +0200

    insertion sort

--- GetAllExpressionsInBrackets/Program.cs
+++ GetAllExpressionsInBrackets/Program.cs
@@ -13,8 +13,37 @@ namespace GetAllExpressionsInBrackets
         {
             var expression = "1 + (2 - (2 + 2) * 4 / (3 + 1)) * 5";
             var stack = new Stack<int>();
-            
 
+            //WithStack(expression, stack);
+            WithRecursion(expression, 1);
+        }
+
+        private static int WithRecursion(string expression, int index)
+        {
+            if (index >= expression.Length)
+            {
+                return 0;
+            }
+
+
+            if (expression[index] == '(')
+            {
+                int result = WithRecursion(expression, index + 1);
+                int length = result + 1 - index;
+                Console.WriteLine(expression.Substring(index, length));
+
+                return WithRecursion(expression, result + 1);
+            }
+            else if (expression[index] == ')')
+            {
+                return index;
+            }
+
+            return WithRecursion(expression, index + 1);
+        }
+
+        private static void WithStack(string expression, Stack<int> stack)
+        {
             for (int i = 0; i < expression.Length; i++)
             {
                 if (expression[i] == '(')
---
+++ InsertionSort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 2, 4, 0, 8, 1, 5, 55, 66, 101, 1, 2, 4, 3, 7, 11, 44, 33 };
+            var pos = 0;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i - 1] > arr[i])
+                {
+                    pos = i;
+
+                    while (pos > 0 && arr[pos] < arr[pos - 1])
+                    {
+                        var current = arr[pos];
+                        arr[pos] = arr[pos - 1];
+                        arr[pos - 1] = current;
+                        pos--;
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", arr));
+        }
+    }
+}
