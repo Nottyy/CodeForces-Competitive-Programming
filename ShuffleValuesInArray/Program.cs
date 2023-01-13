573df8013f5c2785719a53f54ad2c07ada7fe258
Antonio <antonio.marinovvvv@gmail.com>
2023-01-13 10:49:23 +0200

    shuffle values in array

---
+++ ShuffleValuesInArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShuffleValuesInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(" ",Shuffle(10)));
+        }
+
+        static int[] Shuffle(int n)
+        {
+            var arr = new int[n];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i;
+            }
+
+            var rnd = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var j = rnd.Next() % (i + 1);
+                if (i != j)
+                {
+                    var tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+
+            return arr;
+        }
+    }
+}
