7445adb8969567684591c787861d204be661e434
Antonio <antonio.marinovvvv@gmail.com>
2023-01-16 08:46:20 +0200

    hashset

---
+++ Hashset/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashset
+{
+    public class Cat
+    {
+        private string color;
+        private int dailyFeedings;
+
+        public Cat(string color, int dailyFeedings)
+        {
+            this.color = color;
+            this.dailyFeedings = dailyFeedings;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Cat;
+            return this.color == other.color && this.dailyFeedings == other.dailyFeedings;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.color.GetHashCode() ^ this.dailyFeedings.GetHashCode();
+        }
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var cat1 = new Cat("red", 4);
+            var cat2 = new Cat("red", 4);
+            var cat3 = new Cat("grey", 1);
+
+            Console.WriteLine(cat1.GetHashCode());
+            Console.WriteLine(cat2.GetHashCode());
+
+        }
+    }
+
+    public class HashsetNew<T>
+    {
+        private T[] buffer;
+
+        public HashsetNew()
+        {
+            this.buffer = new T[100];
+        }
+        public void Add(T value)
+        {
+            var hash = (uint)value.GetHashCode();
+            var index = hash % buffer.Length;
+            this.buffer[index] = value;
+        }
+
+        public void Remove(T value)
+        {
+            var hash = (uint)value.GetHashCode();
+            var index = hash % buffer.Length;
+            this.buffer[index] = (T)default;
+        }
+
+        public bool Contains(T value)
+        {
+            var hash = (uint)value.GetHashCode();
+            var index = hash % buffer.Length;
+            return this.buffer[index] != null ? true : false;
+        }
+    }
+}
