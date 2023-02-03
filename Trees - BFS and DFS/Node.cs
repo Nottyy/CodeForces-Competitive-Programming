b16e9d18cb399ad865833f447386ed90c57692f1
Antonio <antonio.marinovvvv@gmail.com>
2023-02-03 22:02:56 +0200

    trees - bfs and dfs with queue and stack

---
+++ Trees - BFS and DFS/Node.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees___BFS_and_DFS
+{
+    public class Node<T>
+    {
+        public T Value { get; private set; }
+
+        public Node<T>[] Children;
+
+        public Node<T> Left
+        {
+            get
+            {
+                return this.Children[0];
+            }
+            set
+            {
+                this.Children[0] = value;
+            }
+        }
+        public Node<T> Right
+        {
+            get
+            {
+                return this.Children[1];
+            }
+            set
+            {
+                this.Children[1] = value;
+            }
+        }
+
+        public Node(T value)
+        {
+            this.Value = value;
+            this.Children = new Node<T>[2];
+        }
+    }
+}
---
+++ Trees - BFS and DFS/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees___BFS_and_DFS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var node0 = new Node<int>(0);
+            var node1 = new Node<int>(1);
+            var node2 = new Node<int>(2);
+            var node3 = new Node<int>(3);
+            var node4 = new Node<int>(4);
+            var node5 = new Node<int>(5);
+
+            node3.Left = node1;
+            node3.Right = node4;
+            node1.Left = node0;
+            node1.Right = node2;
+            node4.Right = node5;
+
+            StackDFS(node3);
+            QueueBFS(node3);
+        }
+
+        private static void StackDFS(Node<int> node3)
+        {
+            var stack = new Stack<Node<int>>();
+            stack.Push(node3);
+
+            var sb = new StringBuilder();
+
+            while (stack.Count > 0)
+            {
+                var element = stack.Pop();
+                sb.Append(element.Value + " ");
+
+                if (element.Left != null)
+                {
+                    stack.Push(element.Left);
+                }
+                if (element.Right != null)
+                {
+                    stack.Push(element.Right);
+                }
+            }
+
+            Console.WriteLine(sb);
+        }
+
+        private static void QueueBFS(Node<int> node3)
+        {
+            var queue = new Queue<Node<int>>();
+            queue.Enqueue(node3);
+
+            var sb = new StringBuilder();
+
+            while (queue.Count > 0)
+            {
+                var element = queue.Dequeue();
+                sb.Append(element.Value + " ");
+
+                if (element.Left != null)
+                {
+                    queue.Enqueue(element.Left);
+                }
+                if (element.Right != null)
+                {
+                    queue.Enqueue(element.Right);
+                }
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
