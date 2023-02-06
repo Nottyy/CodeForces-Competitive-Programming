46485fe9d5652cf99563c6077ed04ef7b9cc4452
Antonio <antonio.marinovvvv@gmail.com>
2023-02-06 13:59:19 +0200

    binary heap, implemented add logic

---
+++ BinaryHeap/BinaryHeap.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryHeap
+{
+    public class BinaryHeap<T> where T : IComparable<T>
+    {
+        private List<T> heap;
+        private Func<T, T, bool> func;
+
+        public T Top => heap[1];
+
+        public int Count => heap.Count - 1;
+
+        public bool IsEmpty => heap.Count == 1;
+
+
+        public BinaryHeap(Func<T, T, bool> cmpFunc)
+        {
+            this.heap = new List<T>();
+            this.heap.Add(default);
+            this.func = cmpFunc;
+        }
+
+
+        public void Add(T value)
+        {
+            heap.Add(value);
+
+            var ind = heap.Count - 1;
+            var parentIndex = ind / 2;
+
+            while (ind > 1 && this.func(value, this.heap[parentIndex]))
+            {
+                var tmp = this.heap[ind];
+                this.heap[ind] = this.heap[parentIndex];
+                this.heap[parentIndex] = tmp;
+
+
+                ind = parentIndex;
+                parentIndex /= 2;
+            }
+        }
+    }
+}
---
+++ BinaryHeap/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryHeap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[18];
+            var rnd = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(100);
+            }
+
+            Console.WriteLine(String.Join(" ", arr));
+
+            var heap = new BinaryHeap<int>((a, b) => a < b);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                heap.Add(arr[i]);
+                Console.Write(heap.Top + " ");
+            }
+
+        }
+    }
+}
