d943552344c653dd80211cbd03688f0723beaa71
Antonio <antonio.marinovvvv@gmail.com>
2023-02-23 18:10:03 +0200

    huffman coding

---
+++ HuffmanCoding/PriorityQ.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinaryHeap
+{
+    public class PriorityQ<T>
+    {
+        private List<T> heap;
+        private Func<T, T, bool> func;
+
+        public T Top => heap[1];
+
+        public int Count => heap.Count - 1;
+
+        public bool IsEmpty => heap.Count == 1;
+
+
+        public PriorityQ(Func<T, T, bool> cmpFunc)
+        {
+            this.heap = new List<T>();
+            this.heap.Add(default);
+            this.func = cmpFunc;
+        }
+        public void Add(T value)
+        {
+            heap.Add(value);
+
+            var ind = heap.Count - 1;
+            var parentIndex = ind / 2;
+
+            while (ind > 1 && this.func(value, this.heap[parentIndex]))
+            {
+                var tmp = this.heap[ind];
+                this.heap[ind] = this.heap[parentIndex];
+                this.heap[parentIndex] = tmp;
+
+
+                ind = parentIndex;
+                parentIndex /= 2;
+            }
+        }
+
+        public T RemoveTop()
+        {
+            var currentIndex = 1;
+            T value = default;
+
+            if (this.Count == 2)
+            {
+                var ifFirstIsSmaller = this.func(this.heap[this.Count - 1], this.heap[this.Count]);
+
+                if (!ifFirstIsSmaller)
+                {
+                    var tmp = this.heap[this.Count - 1];
+                    this.heap[this.Count - 1] = this.heap[this.Count];
+                    this.heap[this.Count] = tmp;
+                }
+            }
+            var curTop = this.Top;
+
+            value = this.heap[this.heap.Count - 1];
+
+            this.heap[currentIndex] = value;
+            this.heap.RemoveAt(this.heap.Count - 1);
+
+            this.HeapifyDown(currentIndex, value);
+
+            return curTop;
+        }
+
+        private void HeapifyDown(int currentIndex, T value)
+        {
+            while (currentIndex * 2 + 1 < this.heap.Count)
+            {
+                var smallerChildIndex = this.func(this.heap[currentIndex * 2], this.heap[currentIndex * 2 + 1]) ?
+                    currentIndex * 2 : currentIndex * 2 + 1;
+
+                if (this.func(this.heap[smallerChildIndex], value))
+                {
+                    var tmp = this.heap[currentIndex];
+                    this.heap[currentIndex] = this.heap[smallerChildIndex];
+                    this.heap[smallerChildIndex] = tmp;
+
+                    currentIndex = smallerChildIndex;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ HuffmanCoding/Program.cs
@@ -0,0 +1,55 @@
+﻿using BinaryHeap;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuffmanCoding
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+
+            var frequencies = new Dictionary<char, int>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                var ch = str[i];
+
+                if (frequencies.ContainsKey(ch))
+                {
+                    frequencies[ch]++;
+                }
+                else
+                {
+                    frequencies[ch] = 1;
+                }
+            }
+
+            var queue = new PriorityQ<Tuple<int, HuffmanTree>>((x, y) => x.Item1 < y.Item1);
+
+            foreach (var x in frequencies)
+            {
+                queue.Add(new Tuple<int, HuffmanTree>(x.Value,
+                    new HuffmanTree(x.Key)));
+            }
+
+            while (queue.Count > 1)
+            {
+                var first = queue.RemoveTop();
+                var second = queue.RemoveTop();
+
+                queue.Add(new Tuple<int, HuffmanTree>(first.Item1 + second.Item1,
+                    new HuffmanTree(first.Item2, second.Item2)));
+            }
+
+            var root = queue.RemoveTop().Item2;
+            root.Dfs();
+        }
+    }
+}
