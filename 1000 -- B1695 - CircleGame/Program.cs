688d390d910aafb96f3039b9e8696ef6b57fc021
Antonio <antonio.marinovvvv@gmail.com>
2023-02-28 19:40:24 +0200

    1000 - greedy - circle game

---
+++ 1000 -- B1695 - CircleGame/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1000____B1695___CircleGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine());
+                var nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                if (n % 2 == 0)
+                {
+                    var smallestInd = Array.IndexOf(nums, nums.Min());
+
+                    if (smallestInd % 2 > 0)
+                    {
+                        Console.WriteLine("Mike");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Joe");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Mike");
+                }
+            }
+        }
+    }
+}
