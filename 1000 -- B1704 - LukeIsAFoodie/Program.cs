128dd729c2c5b3f86695e2570901450661c2dfb3
Antonio <antonio.marinovvvv@gmail.com>
2023-03-01 00:22:11 +0200

    1000 - greedy - luke is a foodie

---
+++ 1000 -- B1704 - LukeIsAFoodie/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1000____B1704___LukeIsAFoodie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                var line1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                var n = line1[0];
+                var x = line1[1];
+
+                var nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+                var counter = 0;
+                int min = nums[0];
+                int max = nums[0];
+
+                for (int j = 1; j < n; j++)
+                {
+                    if (nums[j] > max)
+                    {
+                        max = nums[j];
+                    }
+
+                    if (nums[j] < min)
+                    {
+                        min = nums[j];
+                    }
+
+                    if (max - min > 2 * x)
+                    {
+                        counter++;
+                        min = nums[j];
+                        max = nums[j];
+                    }
+                }
+
+                Console.WriteLine(counter);
+            }
+        }
+    }
+}
